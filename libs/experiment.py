3228f2abcc1b76d4cd571d85eeb1924c2188b568
simozhou <simone.procaccia@gmail.com>
2018-12-04 17:10:20 +0100

    Simone - 04/12/18 - updated query() and built _stream() functions

--- libs/experiment.py
+++ libs/experiment.py
@@ -1,5 +1,4 @@
 import os
-import glob
 import subprocess as subp
 # import dbees
 # import bammer
@@ -24,7 +23,6 @@ class Experiment():
                  gi_list="",
                  threads=1,
                  num_barcodes=12):
-        # TODO strip files from extensions
         self.dirname = dirname
         self.database = dbees.make_db(gi_list)
         self.threads = threads
@@ -32,11 +30,11 @@ class Experiment():
         self.status = None
         for i in range(num_barcodes):
             barcode = 'BC' + str(i)
-            barcode_glob = f"./{barcode}*"
-            self.barcodes[barcode] = glob.glob(barcode_glob)
+            self.barcodes[barcode] = list()
 
-    def query(self):
+    def query(self, filename):
         """
+        :param filename: string, $(basename filename) stripped extension fastq
         function that:
         - prepares the magic-blast query
         - transform the sam output into bam
@@ -45,11 +43,11 @@ class Experiment():
         - reset the status to ready when finished
         """
         self.status = "processing"
-        for barcode, filelist in self.barcodes.items():
-            for filename in filelist:
-                file_query = f'magicblast -query ./{filename} -db {self.database} -outfmt sam | ' \
-                             f'samtools view -Sb - > {self.dirname}/bams/{filename}.bam'
-                magicblastsam = subp.check_output(file_query, shell=True)
+
+        # launch the magicblast query for the specific file
+        file_query = f'magicblast -query ./{filename} -db {self.database} -outfmt sam | ' \
+                     f'samtools view -Sb -o {self.dirname}/bams/{filename}.bam -'
+        magicblast_sam = subp.check_output(file_query, shell=True)
 
         self._stream()
 
@@ -57,4 +55,12 @@ class Experiment():
 
 
     def _stream(self):
-        pass
+        """
+        This funtion takes in bams from /bams/newbams
+        for each file in the directory
+        calls bammer.hit_counter(self.barcodes)
+        yields the dictionary produced by hit_counter
+        :return:
+        """
+
+        yield bammer.hit_counter(self.barcodes)
