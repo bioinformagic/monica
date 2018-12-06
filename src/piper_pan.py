7ed1cfeee2c60e2b214375fa907dfbaae8ccff69
jaydee220 <jan.delshad@outlook.com>
2018-12-06 16:13:31 +0100

    update shell_runner

--- src/piper_pan.py
+++ src/piper_pan.py
@@ -5,18 +5,25 @@ __doc__="""Main python file for calling and maintaining the pipeline.
 
 import subprocess
 from subprocess import PIPE
+import sys
 
 def read_MK_info():
 
     # function to read the directory information of the user_conf (json)
-    return somedata
+    return None
 
 def run_deepbinner(args, inp_folder):
     #Function to run deepbinner from bash --> giving with it selected args
     #deepbinner command creator
-
+    return None
 
 
 def shell_runner(cmd):
-    command = subprocess.Popen(cmd, shell=True, executable='/bin/bash', stderr=PIPE, stdout=PIPE)
-    return  command.communicate()
+    try:
+        command = subprocess.check_output(cmd, shell=True, executable='/bin/bash')
+        command = command.decode("utf-8").split(sep="\n")
+        return command
+    except subprocess.CalledProcessError as e:
+        print(e.ouput)
+        sys.exit(1)
+
